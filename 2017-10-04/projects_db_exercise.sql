b1e8e27843b6a75fa8e5a49f9d82be3dc6545483
Alexander <sanchez.reyes2501@gmail.com>
2017-10-05 08:59:10 -0500

    Project DB Exercise 2017-10-04

---
+++ 2017-10-04/projects_db_exercise.sql
@@ -0,0 +1,114 @@
+-- PROJECTS DB EXERCISE
+
+-- 1.
+
+SELECT * FROM project_uses_tech
+	WHERE project_uses_tech.tech_id = 3;
+	
+-- 2.
+
+SELECT * FROM project_uses_tech
+	WHERE project_uses_tech.project_id = 4;
+	
+-- 3.
+
+SELECT * FROM tech
+  LEFT OUTER JOIN project_uses_tech
+    ON tech.id = project_uses_tech.tech_id; -- Ruby, JavaScript and Java have no associated projects
+
+-- 4.
+
+SELECT name, count(tech_id)
+	FROM tech
+	LEFT OUTER JOIN project_uses_tech
+	ON project_uses_tech.tech_id = tech.id
+	GROUP BY tech.id;
+    
+-- 5.
+
+SELECT DISTINCT(name), count(tech_id) AS nums_tech_used FROM project
+  LEFT OUTER JOIN project_uses_tech
+    ON project.id = project_uses_tech.project_id
+    GROUP BY project.id; -- Whiteboard Exercises has no associated projects
+    
+-- 6.
+
+SELECT DISTINCT(name), count(tech_id) AS nums_tech_used FROM project
+  LEFT OUTER JOIN project_uses_tech
+    ON project.id = project_uses_tech.project_id
+    GROUP BY project.id;
+    
+-- 7.
+
+SELECT project.name AS project, tech.name AS techs_used FROM project
+	LEFT OUTER JOIN project_uses_tech
+		ON project_uses_tech.project_id = project.id
+	LEFT OUTER JOIN tech
+		ON project_uses_tech.tech_id = tech.id 
+	ORDER BY project.id, tech.name;
+
+-- 8.
+
+SELECT DISTINCT(tech.name) AS techs_used FROM tech
+	RIGHT OUTER JOIN project_uses_tech
+		ON project_uses_tech.tech_id = tech.id
+	ORDER BY tech.name;
+
+-- 9.
+
+SELECT DISTINCT(tech.name) AS tech_not_used FROM project
+	RIGHT OUTER JOIN project_uses_tech
+		ON project_uses_tech.project_id = project.id
+	RIGHT OUTER JOIN tech
+		ON project_uses_tech.tech_id = tech.id
+	WHERE project_id IS NULL;
+	
+-- 10.
+
+SELECT DISTINCT(project.name) AS projects_with_tech FROM project
+	RIGHT OUTER JOIN project_uses_tech
+		ON project_uses_tech.project_id = project.id
+	WHERE project_uses_tech.tech_id IS NOT NULL;
+	
+-- 11.
+
+SELECT name AS projects_without_tech FROM project
+	LEFT OUTER JOIN project_uses_tech
+		ON project_uses_tech.project_id = project.id
+	WHERE project_uses_tech.tech_id IS NULL;
+	
+-- 12.
+
+SELECT DISTINCT(name), count(tech_id) AS nums_tech_used FROM project
+  LEFT OUTER JOIN project_uses_tech
+    ON project.id = project_uses_tech.project_id
+    GROUP BY project.name
+    ORDER BY nums_tech_used;
+    
+-- 13.
+
+SELECT DISTINCT(name), count(project_id) AS num_projects_using FROM tech
+	LEFT OUTER JOIN project_uses_tech
+		ON tech.id = project_uses_tech.tech_id
+	GROUP BY tech.name
+	ORDER BY num_projects_using;
+
+-- 14.
+
+SELECT DISTINCT(name), count(tech_id) AS num_tech_used FROM project
+	LEFT OUTER JOIN project_uses_tech
+		ON project.id = project_uses_tech.project_id
+		GROUP BY project.name
+		ORDER BY num_tech_used;
+		
+SELECT
+	AVG(count)
+FROM (SELECT
+	DISTINCT(name),
+	count(tech_id)
+FROM
+	project
+INNER JOIN project_uses_tech
+	ON project.id = project_uses_tech.project_id
+	GROUP BY project.name
+	ORDER BY;
